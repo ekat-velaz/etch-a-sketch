f84f80a9985c1d1f7425a4ed98e87f0aefe7e5a3
ekaterina <katyabalash@mail.ru>
2022-10-08 20:04:41 +0300

    Add buttons and erase listener

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const container = document.querySelector(".squareContainer");
 const colorPicker = document.querySelector("#colorPicker");
 let selectedColor = colorPicker.value;
 
+const eraser = document.querySelector(".btn-erase");
+const cell = document.querySelector(".btn-cell");
+const clearGrid = document.querySelector(".btn-clear");
+
+const colorPickerBgr = document.querySelector("#colorPickerBgr");
+let bgColor = colorPickerBgr.value;
+
 function createDivs(squareCount) {
     let squareCountMultiplied = squareCount * squareCount;
     for (let i = 0; i < squareCountMultiplied; i++) {
@@ -24,12 +31,19 @@ function createDivs(squareCount) {
         square.addEventListener('mouseup', () => {
         drawing = false;
         });
+        bgColor = colorPickerBgr.value;
+    container.style.backgroundColor = bgColor;
         
+    
 };
 };
 
 
 createDivs(30);
+
+eraser.addEventListener('click', () => {
+    selectedColor = "transparent";
+});
 
 const range = document.querySelector("#range");
 let rangeValue = document.querySelector("#rangeValue");
@@ -49,4 +63,16 @@ colorPicker.addEventListener("input", () => {
     selectedColor = colorPicker.value;
 });
 
+colorPickerBgr.addEventListener("input", () => {
+    bgColor = colorPickerBgr.value;
+    container.style.backgroundColor = bgColor;
+});
 
+clearGrid.addEventListener("click", () => {
+    
+});
+
+
+cell.addEventListener("click", () => {
+    square.style.borderColor = "transparent";
+});
